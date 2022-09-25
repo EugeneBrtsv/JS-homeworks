e0400d46b45d03bad92d9f36f9be1aedca3cfb2f
EugeneBrtsv <114026406+EugeneBrtsv@users.noreply.github.com>
2022-09-26 00:39:08 +0300

    Add Lesson 1

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -1,1 +1,105 @@
-console.log("Hello, World 1!");
+//1
+
+let str = "Привет";
+let sum = 123;
+let num = 15.8;
+let flag = true;
+let txt = "true";
+
+console.log(typeof str, typeof sum, typeof num, typeof flag, typeof txt);
+
+//2
+let a1 = 5 % 3;
+console.log(typeof a1, a1);
+let a2 = 3 % 5;
+console.log(typeof a2, a2);
+let a3 = 5 + "3";
+console.log(typeof a3, a3);
+let a4 = "5" - 3;
+console.log(typeof a4, a4);
+let a5 = 75 + "кг";
+console.log(typeof a5, a5);
+let a6 = 785 * 653;
+console.log(typeof a6, a6);
+let a7 = 100 / 25;
+console.log(typeof a7, a7);
+let a8 = 0 * 0;
+console.log(typeof a8, a8);
+let a9 = 0 / 2;
+console.log(typeof a9, a9);
+let a10 = 89 / 0;
+console.log(typeof a10, a10);
+let a11 = 98 + 2;
+console.log(typeof a11, a11);
+let a12 = 5 - 98;
+console.log(typeof a12, a12);
+let a13 = (8 + 56 * 4) / 5;
+console.log(typeof a13, a13);
+let a14 = ((9 - 12) * 7) / (5 + 2);
+console.log(typeof a14, a14);
+let a15 = +"123";
+console.log(typeof a15, a15);
+let a16 = 1 || 0;
+console.log(typeof a16, a16);
+let a17 = true || false;
+console.log(typeof a17, a17);
+let a18 = true > 0;
+console.log(typeof a18, a18);
+
+//3
+
+let height = 10;
+let width = 23;
+let SPryam = height * width;
+console.log(SPryam);
+
+//4
+
+let heightСylinder = 10;
+let square = a7;
+let VCilindra = heightСylinder * square;
+console.log(VCilindra);
+
+//5
+
+let r = 5 ** 2;
+const long = 3.14;
+let Skruga = r * long;
+console.log(Skruga);
+
+//6
+
+let a = 5;
+let b = 7;
+let h = 10;
+let STrap = ((a + b) / 2) * h;
+console.log(STrap);
+
+//7
+
+let s = 2000000;
+let p = 10;
+let years = 5;
+let Pereplata = (s / p) * years;
+console.log(Pereplata);
+
+//8
+// a+2(x-b)=16;
+a = 8;
+b = 3;
+let x1 = (16 - a + 2 * b) / 2;
+console.log(x1);
+// b(x+15)=a+6x;
+let x2 = (a - b * 15) / (b - 6);
+console.log(x2);
+//x+2x+ax+bx=23780.
+
+//9
+let first = "Бывало, спит у ног собака, ";
+console.log(first);
+let second = "костер занявшийся гудит,";
+console.log(second);
+let third = "и женщина из полумрака";
+console.log(third);
+let fourth = "глазами зыбкими глядит.";
+console.log(fourth);
