bd924d1fd8040632f17c4427d2e7e41b8030c9b0
EugeneBrtsv <114026406+EugeneBrtsv@users.noreply.github.com>
2022-09-28 22:41:14 +0300

    Add Lesson 3 (1-5)

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -1,1 +1,44 @@
-console.log("Hello, World 3!");
+// //1
+
+for (let a = 1; a <= 50; a++) console.log(a);
+for (let b = 35; b >= 8; b--) console.log(b);
+
+//2
+let i = 89;
+while (i > 11) {
+  i--;
+  console.log(i);
+}
+//3
+let c = 100;
+let sum = 0;
+
+for (let c = 0; c <= 100; c++) {
+  sum += c;
+}
+console.log(sum);
+
+//4
+let d = 5;
+let sum2 = 0;
+
+for (let d = 0; d <= 5; d++) {
+  sum2 += d;
+}
+console.log(sum2);
+
+//5
+for (i = 8; i <= 56; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+let f = 8;
+while (f <= 56) {
+  if (f % 2 === 0) {
+    console.log(f);
+  }
+  f++;
+}
+
+//6
