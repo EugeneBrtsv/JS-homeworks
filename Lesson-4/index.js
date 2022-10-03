bf71d25a6abc6606aeacfdfd4f772c409f9a5d63
EugeneBrtsv <114026406+EugeneBrtsv@users.noreply.github.com>
2022-10-03 18:45:45 +0300

    Add Lesson 4

--- Lesson-4/index.js
+++ Lesson-4/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Word");
+//1
+// let arr = [1, 2, 3, 4, 5];
+// for (i = 0; i < arr.length; i++) {
+//   document.write(arr[i]);
+// }
+
+//2
+// let arr2 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+// for (i = 0; i < arr2.length; i++) {
+//   if (arr2[i] > -10 && arr2[i] < -3) {
+//     console.log(arr2[i]);
+//   }
+// }
